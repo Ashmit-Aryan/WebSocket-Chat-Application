4d2843550378279301db82fdc7a348141318e14a
Ashmit Aryan <ashmitbhatt19@gmail.com>
2026-03-04 19:45:31 +0530

    cookie bug fixed #2

--- server/lib/jwt.js
+++ server/lib/jwt.js
@@ -1,21 +1,24 @@
 import jwt from "jsonwebtoken";
 import * as dotenv from "dotenv";
 dotenv.config();
-export const generateToken = (userId,res) => {
-    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: "7d" });
-    res.cookie("token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "development" ? false : true,
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 1 hour
-        sameSite: "none"
-    });
-    return token;
+export const generateToken = (userId, res) => {
+  const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: false, // because HTTP
+    sameSite: "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    path: "/",
+  });
+  return token;
 };
 
 export const verifyToken = (token) => {
-    try {
-        return jwt.verify(token, process.env.JWT_SECRET);
-    } catch (error) {
-        return null;
-    }
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return null;
+  }
 };
